aa7345ce6b04018e88d226427d7ab238a3a30559
chentingyang <37823132+chentingyang@users.noreply.github.com>
2022-10-14 23:18:58 +0800

    Update dataset_NY_his.py

--- NYC-OD/utils/dataset_NY_his.py
+++ NYC-OD/utils/dataset_NY_his.py
@@ -98,9 +98,6 @@ def load_data(odmax, timestep, scaler=True):
     print("*************************")
     print("load data done")
     print("*************************")
-    print("*************************")
-    print("generate sequence")
-    print("*************************")
 
     sets = len(oddata.keys())
 
@@ -156,8 +153,8 @@ def load_data_seq(odmax, timestep, seq_out_len, scaler=True):
         s = (timestep, map_height * map_width, map_height * map_width), semantic neb_matrix sequence
         geo = (map_height * map_width, map_height * map_width), adjacency neb_matrix
     '''
-    oddata = '../data/oddata.npy'
-    weather = '../data/weather.npy'
+    oddata = './data/oddata.npy'
+    weather = './data/weather.npy'
 
     print("*************************")
     print("load data")

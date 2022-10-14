3095902881631f61b9817f05d4f729fa87ca9f73
chentingyang <37823132+chentingyang@users.noreply.github.com>
2022-10-14 23:23:42 +0800

    Update dataset_NY_his.py

--- NYC-OD/utils/dataset_NY_his.py
+++ NYC-OD/utils/dataset_NY_his.py
@@ -137,10 +137,6 @@ def load_data(odmax, timestep, scaler=True):
     w = np.concatenate(w)
     s = np.concatenate(s)
 
-    print("*************************")
-    print("generate sequence done")
-    print("*************************")
-
     return o, y, s, geo, w
 
 

919a9c36389fda468922e388447e764817ce6c0a
chentingyang <37823132+chentingyang@users.noreply.github.com>
2022-10-14 11:34:55 +0800

    Add files via upload

--- HAIKOU-OD/utils/dataset_HKsmall_his.py
+++ HAIKOU-OD/utils/dataset_HKsmall_his.py
@@ -85,7 +85,7 @@ def load_data(odmax, timestep, scaler=True):
         s = (timestep, map_height * map_width, map_height * map_width), semantic neb_matrix sequence
         geo = (map_height * map_width, map_height * map_width), adjacency neb_matrix
     '''
-    oddata = '../data/oddatabig.npy'
+    oddata = '../data/oddatasmall.npy'
     weather = '../data/weather.npy'
 
     print("*************************")
@@ -131,6 +131,77 @@ def load_data(odmax, timestep, scaler=True):
 
         o.append(np.concatenate([oddata_set[T + i:i - timestep, newaxis, ...] for i in range(timestep)], axis=1))
         y.append(oddata_set[T + timestep:, ...])
+        w.append(np.concatenate([weather_set[T + i:i - timestep, newaxis, ...] for i in range(timestep)], axis=1))
+        s.append(np.concatenate([semantic[T + i:i - timestep, newaxis, ...] for i in range(timestep)], axis=1))
+
+    o = np.concatenate(o)
+    y = np.concatenate(y)
+    w = np.concatenate(w)
+    s = np.concatenate(s)
+
+    print("*************************")
+    print("generate sequence done")
+    print("*************************")
+
+    return o, y, s, geo, w
+
+
+def load_data(odmax, timestep, seq_out_len, scaler=True):
+    '''
+        expectation:
+        o = (sample, timestep, map_height * map_width, map_height, map_width), od data sequence
+        y = (sample, seq_out_len, map_height * map_width, map_height, map_width), ground_truth
+        w = (sample, timestep, ?), meterological data sequence
+        s = (timestep, map_height * map_width, map_height * map_width), semantic neb_matrix sequence
+        geo = (map_height * map_width, map_height * map_width), adjacency neb_matrix
+    '''
+    oddata = '../data/oddatasmall.npy'
+    weather = '../data/weather.npy'
+
+    print("*************************")
+    print("load data")
+    print("*************************")
+    oddata = np.load(oddata, allow_pickle=True, encoding='bytes')[()]
+    weather = np.load(weather, allow_pickle=True, encoding='bytes')[()]
+
+    print("*************************")
+    print("load data done")
+    print("*************************")
+    print("*************************")
+    print("generate sequence")
+    print("*************************")
+
+    sets = len(oddata.keys())
+
+    # generate semantic neb_matrix (based on the bidirectional traffic flow)
+    data = np.array(oddata[0])
+    data = np.reshape(data, (-1, N, N))
+    semantic = []
+    for graph in data:
+        semantic.append(generate_dynamic_adj_matrix(graph))
+
+    semantic = np.array(semantic)
+
+    # generate adjacency neb_matrix
+    geo = generate_adj_matrix()
+
+    if scaler:
+        for i in oddata.keys():
+            oddata[i] = oddata[i] * 2.0 / odmax - 1.0  # (-1, 1)
+            # oddata[i] = (oddata[i] - mean) / std  #standardscaler
+
+    o = []
+    w = []
+    y = []
+    s = []
+
+    for i in oddata.keys():
+        oddata_set = oddata[i]
+        weather_set = weather[i]
+
+        o.append(np.concatenate([oddata_set[T + i:i - timestep, newaxis, ...] for i in range(timestep)], axis=1))
+        y.append( \
+            np.concatenate([oddata_set[T+i+timestep:i-timestep, newaxis, ...] for i in range(seq_out_len)], axis=1))
         w.append(np.concatenate([weather_set[T + i:i - timestep, newaxis, ...] for i in range(timestep)], axis=1))
         s.append(np.concatenate([semantic[T + i:i - timestep, newaxis, ...] for i in range(timestep)], axis=1))
 
